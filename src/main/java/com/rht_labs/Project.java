fc77ee6357337d668b999c79901cb40e0e11cbec
Jacob See <jasee@redhat.com>
2019-11-05 22:45:33 -0800

    Initial commit

---
+++ src/main/java/com/rht_labs/Project.java
@@ -0,0 +1,16 @@
+package com.rht_labs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/project")
+public class Project {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "{\"thing\": \"hello\"}";
+    }
+}
---
+++ src/test/java/com/rht-labs/NativeProjectIT.java
@@ -0,0 +1,9 @@
+package com.rht_labs;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeProjectIT extends ProjectTest {
+
+    // Execute the same tests but in native mode.
+}
