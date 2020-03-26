f6acb5511d5089086e2380fcbae1a4168cef535e
Derek Wasinger <derekwasinger@dereks-mbp.lan>
2020-03-26 11:36:17 -0700

    removed tmp script

--- set-local-env.sh
+++
@@ -1,7 +0,0 @@
-export JWT_PUBKICKEY_LOCATION=https://sso-omp-jasee.apps.s11.core.rht-labs.com/auth/realms/omp/protocol/openid-connect/certs
-export JWT_ISSUER=https://sso-omp-jasee.apps.s11.core.rht-labs.com/auth/realms/omp
-export JWT_ENABLE=true
-
-export OMP_GITLAB_API_URL=https://omp-git-api-omp-dev.apps.s11.core.rht-labs.com
-
-export CACHE_SERVICE=127.0.0.1
