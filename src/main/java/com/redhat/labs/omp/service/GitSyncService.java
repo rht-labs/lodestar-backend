55787489421c23763faa55948ae606ac997cbc16
Derek Wasinger <dwasinge@redhat.com>
2020-05-08 09:24:38 -0700

    bug fix for active, refactor, and remove save toggle (#63)

--- src/main/java/com/redhat/labs/omp/service/GitSyncService.java
+++ src/main/java/com/redhat/labs/omp/service/GitSyncService.java
@@ -1,9 +1,6 @@
 package com.redhat.labs.omp.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -14,22 +11,15 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.redhat.labs.omp.model.ActiveSync;
 import com.redhat.labs.omp.model.Engagement;
 import com.redhat.labs.omp.model.FileAction;
 import com.redhat.labs.omp.repository.ActiveSyncRepository;
 import com.redhat.labs.omp.rest.client.OMPGitLabAPIService;
 
-import io.quarkus.panache.common.Sort;
-import io.quarkus.scheduler.Scheduled;
-
 @ApplicationScoped
 public class GitSyncService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GitSyncService.class);
-    private final UUID uuid = UUID.randomUUID();
-
-    AtomicBoolean autosave = new AtomicBoolean(true);
 
     @Inject
     ActiveSyncRepository activeSyncRepository;
@@ -40,36 +30,6 @@ public class GitSyncService {
     @Inject
     @RestClient
     OMPGitLabAPIService gitApiClient;
-
-    /**
-     * Periodically processes modified {@link Engagement} in the data store by
-     * calling the Git API to update Git.
-     */
-    @Scheduled(cron = "{auto.save.cron.expr}")
-    void sendChangesToGitApi() {
-
-        if (autosave.get() && active()) {
-
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("backend instance {} is active and performing push to git.", uuid);
-            }
-
-            processModifiedEngagements();
-
-        }
-
-    }
-
-    public boolean toggleAutoSave() {
-
-        boolean temp;
-        do {
-            temp = autosave.get();
-        } while (!autosave.compareAndSet(temp, !temp));
-
-        return autosave.get();
-
-    }
 
     public void refreshBackedFromGit() {
 
@@ -82,61 +42,6 @@ public class GitSyncService {
         engagementService.syncWithGitLab(engagementList);
 
         LOGGER.info("refresh of backend data complete.");
-
-    }
-
-    /**
-     * Return true if this application instance is performing the sync processing.
-     * Will make itself active if the record goes stale in the db. Otherwise, will
-     * return false to indicate it should not be processing.
-     * 
-     * @return
-     */
-    boolean active() {
-
-        // get record from db
-        List<ActiveSync> syncRecordList = activeSyncRepository.listAll(Sort.ascending("lastUpdated"));
-
-        if (syncRecordList.size() == 0) {
-
-            // try to insert the record to become the active instance
-            activeSyncRepository.persist(new ActiveSync(uuid, LocalDateTime.now()));
-            return true;
-
-        }
-
-        // get control record
-        ActiveSync sync = syncRecordList.remove(0);
-
-        // shouldn't happen, but remove extra records if more than one exists
-        if (syncRecordList.size() > 0) {
-
-            for (ActiveSync record : syncRecordList) {
-                activeSyncRepository.delete(record);
-            }
-
-        }
-
-        // if i created the record, update the time stamp
-        if (uuid.equals(sync.getUuid())) {
-
-            // i am active, update time stamp
-            sync.setLastUpdated(LocalDateTime.now());
-            activeSyncRepository.update(sync);
-            return true;
-
-        }
-
-        // i didn't create it, but check if last updated is stale
-        if (sync.getLastUpdated().isBefore(LocalDateTime.now().minusMinutes(1))) {
-
-            sync.setUuid(uuid);
-            activeSyncRepository.persistOrUpdate(sync);
-            return true;
-
-        }
-
-        return false;
 
     }
 
