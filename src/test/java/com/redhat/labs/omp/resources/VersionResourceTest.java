39fae64798ec8c4fab811677fffd93525a146072
Derek Wasinger <dwasinge@redhat.com>
2020-07-22 09:25:43 -0700

    added test for version summary api

--- src/test/java/com/redhat/labs/omp/resources/VersionResourceTest.java
+++ src/test/java/com/redhat/labs/omp/resources/VersionResourceTest.java
@@ -2,6 +2,7 @@ package com.redhat.labs.omp.resources;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,22 @@ public class VersionResourceTest {
             .body("containers.version",hasItem("master-abcdef"))
             .body("containers.version", hasItem("v1.1"));
     }   
+
+    @Test
+    public void testResourceVersionSummary() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .get("/api/v1/version/summary")
+        .then()
+            .statusCode(200)
+            .body("main_version.name", is("ball"))
+            .body("main_version.value", is("v2.0"))
+            .body("component_versions.name", hasItem("launcher"))
+            .body("component_versions.value", hasItem("v1.1"));
+
+    }
+
 }
     
