067a01f451d181fc283d28bba89dc02ed11aac10
Derek Wasinger <dwasinge@redhat.com>
2020-08-04 12:08:23 -0700

    Expose Status Service Version through Backend (#93)
    * added version manifest api to version resource
    
    * added deprecation for original version service
    
    * versions config map now in lodestar-status
    
    * add missing link address attribute
    
    * changed version to use header and associated tests
    
    * remove version from path, should use header in v2

---
+++ src/main/java/com/redhat/labs/omp/model/status/VersionV1.java
@@ -0,0 +1,21 @@
+package com.redhat.labs.omp.model.status;
+
+import javax.json.bind.annotation.JsonbProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VersionV1 {
+
+    private String name;
+    private String value;
+    @JsonbProperty("link_address")
+    private String linkAddress;
+
+}
--- src/test/java/com/redhat/labs/omp/resources/VersionResourceTest.java
+++ src/test/java/com/redhat/labs/omp/resources/VersionResourceTest.java
@@ -2,6 +2,7 @@ package com.redhat.labs.omp.resources;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,79 @@ public class VersionResourceTest {
             .body("containers.version",hasItem("master-abcdef"))
             .body("containers.version", hasItem("v1.1"));
     }   
+
+    @Test
+    public void testValidResourceVersion1() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .header("Accept-version", "v1")
+            .get("/api/version")
+        .then()
+            .statusCode(200)
+            .statusCode(200)
+            .body("containers.version",hasItem("master-abcdef"))
+            .body("containers.version", hasItem("v1.1"));
+
+    }
+
+    @Test
+    public void testValidResourceVersion2() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .header("Accept-version", "v2")
+            .get("/api/version")
+        .then()
+            .statusCode(200)
+            .body("git_commit", is("abcdef"))
+            .body("git_tag", is("master"));
+
+    }
+
+    @Test
+    public void testValidResourceVersionNoAcceptVersionSupplied() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .get("/api/version")
+        .then()
+            .statusCode(200)
+            .body("git_commit", is("abcdef"))
+            .body("git_tag", is("master"));
+
+    }
+
+    @Test
+    public void testValidResourceVersionInvalidAcceptVersion() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .header("Accept-version", "v8")
+            .get("/api/version")
+        .then()
+            .statusCode(400);
+
+    }
+
+    @Test
+    public void testValidResourceVersionManifest() {
+
+        given()
+        .when()
+            .contentType(ContentType.JSON)
+            .get("/api/version/manifest")
+        .then()
+            .statusCode(200)
+            .body("main_version.name",is("ball"))
+            .body("main_version.value", is("v2.0"))
+            .body("component_versions.name", hasItem("launcher"))
+            .body("component_versions.value", hasItem("v1.1"));
+
+    }
+
 }
-    
