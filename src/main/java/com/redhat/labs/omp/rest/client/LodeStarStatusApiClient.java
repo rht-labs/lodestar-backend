bd9400ecf4e7d5b8687391f0c866efb94c586f2b
Derek Wasinger <dwasinge@redhat.com>
2020-08-19 09:30:33 -0700

    cleanup

--- src/main/java/com/redhat/labs/omp/rest/client/LodeStarStatusApiClient.java
+++ src/main/java/com/redhat/labs/omp/rest/client/LodeStarStatusApiClient.java
@@ -6,9 +6,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import com.redhat.labs.omp.model.status.VersionManifestV1;
@@ -23,12 +20,8 @@ public interface LodeStarStatusApiClient {
     public VersionManifestV1 getVersionManifestV1();
 
     @GET
-    @Produces("application/json")
     @Path("/api/v1/status")
-    @APIResponses(value = { 
-            @APIResponse(responseCode = "401", description = "Missing or Invalid JWT"),
-            @APIResponse(responseCode = "200", description = "Configuration file data has been returned.") })
-    @Operation(summary = "Returns configuration file data from git.")
+    @Produces("application/json")
     public Response getComponentStatus();
 
 }
