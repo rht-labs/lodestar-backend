4caab83fddafe4262727cce94529a77d2ad2fbb4
Derek Wasinger <dwasinge@redhat.com>
2020-08-19 10:10:44 -0700

    adding liveness and readiness checks

---
+++ src/main/java/com/redhat/labs/omp/health/GitApiHealthCheck.java
@@ -0,0 +1,32 @@
+package com.redhat.labs.omp.health;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import com.redhat.labs.omp.rest.client.OMPGitLabAPIService;
+
+@Readiness
+@ApplicationScoped
+public class GitApiHealthCheck implements HealthCheck {
+
+    private static final String NAME = "Git API";
+    @Inject
+    OMPGitLabAPIService service;
+
+    @Override
+    public HealthCheckResponse call() {
+
+        try {
+            service.getVersion();
+            return HealthCheckResponse.up(NAME);
+        } catch (Exception e) {
+            return HealthCheckResponse.down(NAME);
+        }
+
+    }
+
+}
