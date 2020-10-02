1492dc90cefc266134f146a9d46f691ada608413
Derek Wasinger <dwasinge@redhat.com>
2020-10-02 13:04:51 -0700

    added use cases to engagement (#113)

---
+++ src/main/java/com/redhat/labs/lodestar/model/UseCase.java
@@ -0,0 +1,19 @@
+package com.redhat.labs.lodestar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UseCase {
+
+    private String id;
+    private String title;
+    private String description;
+    private Integer order;
+
+}
