8e1838f63d0a3975591edd10c15340db71459441
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-11-12 11:08:26 -0700

    indented hosting environment correctly

--- src/main/java/com/redhat/labs/lodestar/model/HostingEnvironment.java
+++ src/main/java/com/redhat/labs/lodestar/model/HostingEnvironment.java
@@ -13,31 +13,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class HostingEnvironment {
 
-  @JsonbProperty("id")
-  private String id;
+    @JsonbProperty("id")
+    private String id;
 
-  @JsonbProperty("environment_name")
-  private String environmentName;
+    @JsonbProperty("environment_name")
+    private String environmentName;
 
-  @JsonbProperty("additional_details")
-  private String additionalDetails;
+    @JsonbProperty("additional_details")
+    private String additionalDetails;
 
-  @JsonbProperty("ocp_cloud_provider_name")
-  private String ocpCloudProviderName;
+    @JsonbProperty("ocp_cloud_provider_name")
+    private String ocpCloudProviderName;
 
-  @JsonbProperty("ocp_cloud_provider_region")
-  private String ocpCloudProviderRegion;
+    @JsonbProperty("ocp_cloud_provider_region")
+    private String ocpCloudProviderRegion;
 
-  @JsonbProperty("ocp_persistent_storage_size")
-  private String ocpPersistentStorageSize;
+    @JsonbProperty("ocp_persistent_storage_size")
+    private String ocpPersistentStorageSize;
 
-  @JsonbProperty("ocp_sub_domain")
-  private String ocpSubDomain;
+    @JsonbProperty("ocp_sub_domain")
+    private String ocpSubDomain;
 
-  @JsonbProperty("ocp_version")
-  private String ocpVersion;
+    @JsonbProperty("ocp_version")
+    private String ocpVersion;
 
-  @JsonbProperty("ocp_cluster_size")
-  private String ocpClusterSize;
+    @JsonbProperty("ocp_cluster_size")
+    private String ocpClusterSize;
 
 }
