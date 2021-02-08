3caad29534010d69336f6981d8de4d35823ee53a
Derek Wasinger <dwasinge@redhat.com>
2021-02-08 16:20:27 -0700

    initial retry

--- src/main/java/com/redhat/labs/lodestar/model/event/EventType.java
+++ src/main/java/com/redhat/labs/lodestar/model/event/EventType.java
@@ -2,10 +2,17 @@ package com.redhat.labs.lodestar.model.event;
 
 public class EventType {
 
+    private EventType() {
+        throw new IllegalStateException("Utility class");
+    }
+    
     public static final String CREATE_ENGAGEMENT_EVENT_ADDRESS = "create.engagement.event";
     public static final String UPDATE_ENGAGEMENT_EVENT_ADDRESS = "update.engagement.event";
     public static final String DELETE_ENGAGEMENT_EVENT_ADDRESS = "delete.engagement.event";
     public static final String REFRESH_DATABASE_EVENT_ADDRESS = "refresh.database.event";
     public static final String SET_UUID_EVENT_ADDRESS = "set.uuid.event";
+    public static final String RETRY_CREATE_EVENT_ADDRESS = "retry.create.event";
+    public static final String RETRY_UPDATE_EVENT_ADDRESS = "retry.update.event";
+    public static final String RETRY_DELETE_EVENT_ADDRESS = "retry.delete.event";
 
 } 
--- src/main/java/com/redhat/labs/lodestar/service/EventService.java
+++ src/main/java/com/redhat/labs/lodestar/service/EventService.java
@@ -1,22 +1,32 @@
 package com.redhat.labs.lodestar.service;
 
+import java.util.Optional;
+
 import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.redhat.labs.lodestar.model.Engagement;
+import com.redhat.labs.lodestar.model.EngagementUser;
 import com.redhat.labs.lodestar.model.event.EventType;
+import com.redhat.labs.lodestar.model.event.RetriableEvent;
+import com.redhat.labs.lodestar.model.event.RetriableEvent.RetriableEventBuilder;
 import com.redhat.labs.lodestar.rest.client.LodeStarGitLabAPIService;
 
 import io.quarkus.vertx.ConsumeEvent;
+import io.vertx.mutiny.core.eventbus.EventBus;
 
 public class EventService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EventService.class);
+
+    @ConfigProperty(name = "event.max.retries", defaultValue = "-1")
+    Integer eventMaxRetries;
 
     @Inject
     @RestClient
@@ -25,42 +35,140 @@ public class EventService {
     @Inject
     EngagementService engagementService;
 
-    // create engagement
+    @Inject
+    EventBus eventBus;
+
+    /**
+     * Wraps the {@link Engagement} in a {@link RetriableEvent} and starts
+     * processing create API call.
+     * 
+     * @param engagement
+     */
     @ConsumeEvent(value = EventType.CREATE_ENGAGEMENT_EVENT_ADDRESS, blocking = true)
     void consumeCreateEngagementEvent(Engagement engagement) {
-
-        createOrUpdateEngagement(engagement, true);
-        if (null != engagement.getProjectId()) {
-            engagementService.setProjectId(engagement.getUuid(), engagement.getProjectId());
-        }
-
+        RetriableEvent event = buildRetriableEvent(engagement);
+        createOrUpdateEngagement(event, true);
     }
 
-    // update engagement
+    /**
+     * Wraps the {@link Engagement} in a {@link RetriableEvent} and starts
+     * processing update API call.
+     * 
+     * @param engagement
+     */
     @ConsumeEvent(value = EventType.UPDATE_ENGAGEMENT_EVENT_ADDRESS, blocking = true)
     void consumeUpdateEngagementEvent(Engagement engagement) {
-        createOrUpdateEngagement(engagement, false);
+        RetriableEvent event = buildRetriableEvent(engagement);
+        createOrUpdateEngagement(event, false);
     }
 
-    // delete engagement
+    /**
+     * Wraps the {@link Engagement} in a {@link RetriableEvent} and starts
+     * processing the delete API call.
+     * 
+     * @param engagement
+     */
     @ConsumeEvent(EventType.DELETE_ENGAGEMENT_EVENT_ADDRESS)
     void consumeDeleteEngagementEvent(Engagement engagement) {
-        gitApiClient.deleteEngagement(engagement.getCustomerName(), engagement.getProjectName());
+        RetriableEvent event = buildRetriableEvent(engagement);
+        deleteEngagement(event);
     }
 
-    // db refresh
+    /**
+     * Starts the process of inserting any {@link Engagement}s in Git that are not
+     * in the database. Existing {@link Engagement}s will not be udpated.
+     * 
+     * @param event
+     */
     @ConsumeEvent(value = EventType.REFRESH_DATABASE_EVENT_ADDRESS, blocking = true)
     void consumeRefreshDatabaseEvent(String event) {
         engagementService.syncGitToDatabase(false);
     }
 
-    // uuid setting
+    /**
+     * Starts the process of setting UUIDs for any {@link Engagement} or
+     * {@link EngagementUser} that has a null value for UUID.
+     * 
+     * @param event
+     */
     @ConsumeEvent(value = EventType.SET_UUID_EVENT_ADDRESS, blocking = true)
     void setUuidEvent(String event) {
         engagementService.setNullUuids();
     }
 
-    private void createOrUpdateEngagement(Engagement engagement, boolean isCreate) {
+    /**
+     * Sends the given {@link Engagement} to be processed if the max retry limit has
+     * not been exceeded.
+     * 
+     * @param event
+     */
+    @ConsumeEvent(value = EventType.RETRY_CREATE_EVENT_ADDRESS, blocking = true)
+    void consumeRetryCreateEvent(RetriableEvent event) {
+        retryEvent(event, () -> createOrUpdateEngagement(event, true));
+    }
+
+    /**
+     * Sends the given {@link Engagement} to be processed if the max retry limit has
+     * not been exceeded and the {@link Engagement} has not been updated since the
+     * original update request.
+     * 
+     * @param event
+     */
+    @ConsumeEvent(EventType.RETRY_UPDATE_EVENT_ADDRESS)
+    void consumeRetryUpdateEvent(RetriableEvent event) {
+        retryEvent(event, () -> {
+
+            Engagement engagement = event.getEngagement();
+
+            // get current engagement from db
+            Engagement persisted = engagementService.getByUuid(engagement.getUuid(), Optional.empty());
+
+            // only resend if not updated
+            if (persisted.getLastUpdate().equals(engagement.getLastUpdate())) {
+                createOrUpdateEngagement(event, false);
+            }
+
+        });
+
+    }
+
+    /**
+     * Sends the given {@link Engagement} to be processed if the max retry limit has
+     * not been exceeded.
+     * 
+     * @param event
+     */
+    @ConsumeEvent(value = EventType.RETRY_DELETE_EVENT_ADDRESS, blocking = true)
+    void consumeRetryDeleteEvent(RetriableEvent event) {
+        retryEvent(event, () -> deleteEngagement(event));
+    }
+
+    /**
+     * Runs the given function if the event max retry has not been exceeded.
+     * 
+     * @param event
+     * @param runnable
+     */
+    void retryEvent(RetriableEvent event, Runnable runnable) {
+        if (event.shouldRetry()) {
+            runnable.run();
+        }
+    }
+
+    /**
+     * Calls the create or update API with the given {@link Engagement}. If the
+     * isCreate flag is true, it will update the project ID in the database based on
+     * the API response.
+     * 
+     * If the API call produces an error code, the {@link RetriableEvent} will be
+     * sent to the {@link EventBus} for reprocessing.
+     * 
+     * @param event
+     * @param isCreate
+     */
+    private void createOrUpdateEngagement(RetriableEvent event, boolean isCreate) {
+
+        Engagement engagement = event.getEngagement();
 
         try (Response response = gitApiClient.createOrUpdateEngagement(engagement, engagement.getLastUpdateByName(),
                 engagement.getLastUpdateByEmail())) {
@@ -68,13 +176,41 @@ public class EventService {
             if (isCreate) {
                 Integer projectId = getProjectIdFromResponse(response);
                 engagement.setProjectId(projectId);
+                engagementService.setProjectId(engagement.getUuid(), projectId);
             }
 
         } catch (WebApplicationException e) {
             // rest call returned and 400 or above http code
-            LOGGER.error(
-                    "failed to create or update engagement with message '{}', please check to see data needs to be refreshed from git. engagement: {}",
-                    e.getMessage(), engagement);
+            LOGGER.error("failed to create or update engagement with message '{}', engagement: {}", e.getMessage(),
+                    engagement);
+
+            String address = isCreate ? EventType.RETRY_CREATE_EVENT_ADDRESS : EventType.RETRY_UPDATE_EVENT_ADDRESS;
+            event.incrementCurrentRetryCount();
+            eventBus.sendAndForget(address, event);
+
+        }
+
+    }
+
+    /**
+     * Calls the delete API with the given {@link Engagement}.
+     * 
+     * If the API call produces an error code, the {@link RetriableEvent} will be
+     * sent to the {@link EventBus} for reprocessing.
+     * 
+     * @param event
+     */
+    private void deleteEngagement(RetriableEvent event) {
+
+        Engagement engagement = event.getEngagement();
+
+        try {
+            gitApiClient.deleteEngagement(engagement.getCustomerName(), engagement.getProjectName());
+        } catch (WebApplicationException e) {
+
+            LOGGER.error("failed to delete engagement with message {}, engagement: {}", e.getMessage(), engagement);
+            event.incrementCurrentRetryCount();
+            eventBus.sendAndForget(EventType.RETRY_DELETE_EVENT_ADDRESS, event);
 
         }
 
@@ -94,6 +230,25 @@ public class EventService {
         String id = location.substring(location.lastIndexOf("/") + 1);
 
         return Integer.valueOf(id);
+
+    }
+
+    /**
+     * Builds a {@link RetriableEvent} containing the given {@link Engagement}. If
+     * the max retries was configured, it will be used.
+     * 
+     * @param engagement
+     * @return
+     */
+    private RetriableEvent buildRetriableEvent(Engagement engagement) {
+
+        RetriableEventBuilder builder = RetriableEvent.builder().engagement(engagement);
+
+        if (null != eventMaxRetries) {
+            builder.maxRetryCount(eventMaxRetries);
+        }
+
+        return builder.build();
 
     }
 
