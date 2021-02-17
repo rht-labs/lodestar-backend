cf51126e7b9de2dbeb33dd2bf5b65895afba862c
Derek Wasinger <dwasinge@dwasinge-mac.lan>
2021-02-17 12:48:07 -0700

    add back in vm config test

--- src/test/java/com/redhat/labs/lodestar/config/VersionManifestConfigTest.java
+++ src/test/java/com/redhat/labs/lodestar/config/VersionManifestConfigTest.java
@@ -11,23 +11,23 @@ import com.redhat.labs.lodestar.model.VersionManifest;
 
 import io.quarkus.test.junit.QuarkusTest;
 
-//@QuarkusTest
-//@Tag("integration")
+@QuarkusTest
+@Tag("integration")
 class VersionManifestConfigTest {
 
-//    @Inject
-//    VersionManifestConfig vmc;
-//    
-//    @Test
-//    void testYaml() throws JsonProcessingException {
-//        
-//        VersionManifest vm = vmc.getVersionData();
-//
-//        Assertions.assertEquals(1, vm.getContainers().size());
-//        Assertions.assertEquals(2, vm.getApplications().size());
-//        Assertions.assertEquals("lodestar-backend-container", vm.getContainers().get(0).getApplication());
-//        Assertions.assertEquals("master-abcdef", vm.getContainers().get(0).getVersion());
-//        Assertions.assertEquals("ball", vm.getApplications().get(0).getApplication());
-//        Assertions.assertEquals("v2.0", vm.getApplications().get(0).getVersion());
-//    }
+    @Inject
+    VersionManifestConfig vmc;
+    
+    @Test
+    void testYaml() throws JsonProcessingException {
+        
+        VersionManifest vm = vmc.getVersionData();
+
+        Assertions.assertEquals(1, vm.getContainers().size());
+        Assertions.assertEquals(2, vm.getApplications().size());
+        Assertions.assertEquals("lodestar-backend-container", vm.getContainers().get(0).getApplication());
+        Assertions.assertEquals("master-abcdef", vm.getContainers().get(0).getVersion());
+        Assertions.assertEquals("ball", vm.getApplications().get(0).getApplication());
+        Assertions.assertEquals("v2.0", vm.getApplications().get(0).getVersion());
+    }
 }
